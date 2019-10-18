b5ccec038d4f6ea0b02a8b129191b8997aef8be0
Yuxue <54583626+Yuxue-hub@users.noreply.github.com>
2019-10-18 16:27:42 +0800

    C++作业5（课后4、5题）

---
+++ e_2.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+const float pi = 3.14;
+using namespace std;
+void main()
+{
+	int r;
+	cin >> r;
+	cout << "perimeter:" << pi * r << endl;
+	cout << "area:" << pi * r*r << endl;
+	system("pause");
+}
---
+++ e_3.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int *p = new int[20], a = 0, b = 0, i = 0;
+
+	while (cin >> p[i])
+
+	{
+
+		i++;
+
+		if (cin.get() == '\n')
+
+			break;
+
+	}
+	for (int t = 0; t < i; t++)
+	{
+		if (p[t] < 0)
+			a++;
+		if (p[t] > 0)
+			b++;
+		
+	}
+		
+	cout << "正数个数:" << b << endl;
+
+	cout << "负数个数:" << a << endl;
+
+	delete[]p;
+	system("pause");
+	return 0;
+}
